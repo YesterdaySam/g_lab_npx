999c60490e4741eda1a79a6270e67a1c7a85b13e
YesterdaySam <lwilmerd@brandeis.edu>
2024-07-12 10:19:13 -0400

    timestamp alignment and velocity plot
    script version of timestamp alignment.
    Velocity firing rate code

--- readSGLX.m
+++ readSGLX.m
@@ -1,13 +1,16 @@
 
-binName = 'KW001_05302024_rec_D7_g0_t0.imec0.lf.bin';
-binPath = 'D:\Kelton\probe_data\KW001\KW001_05302024_rec_D7_g0\KW001_05302024_rec_D7_g0_imec0\';
+binName = 'KW004_06272024_rec_D1_CA1_g0_t0.imec0.lf.bin';
+binPath = 'D:\Kelton\probe_data\KW004\KW004_06272024_rec_D1_CA1_g0\KW004_06272024_rec_D1_CA1_g0_imec0\';
 meta = SGLX_readMeta.ReadMeta(binName, binPath);
 
 nSamp = floor(str2double(meta.fileTimeSecs) * SGLX_readMeta.SampRate(meta));
 dataArray = SGLX_readMeta.ReadBin(0, nSamp, meta, binName, binPath);
 
 %%
+% For a digital channel: read this digital word dw in the saved file
+% (1-based). For imec data there is never more than one saved digital word.
 dw = 1;
+% For 3B2 imec data: the sync pulse is stored in line 6.
 dLineList = 6;
 
 digArray = SGLX_readMeta.ExtractDigital(dataArray, meta, dw, dLineList);
@@ -16,4 +19,4 @@ digArray = SGLX_readMeta.ExtractDigital(dataArray, meta, dw, dLineList);
 %     hold on
 % end
 
-save('KW001_05302024_sync','digArray')
+save('KW004_06272024_sync','digArray')
